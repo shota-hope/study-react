90474ed5c2a06875d4ac6bda45bbfc93a7a09428
shota-hope <ddeeppaa.gogo@gmail.com>
2022-07-24 11:08:04 +0900

    fic: link change

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Hello <a href="/about">Next.js!</a>
+          Hello Next.js!
         </h1>
 
         <p className={styles.description}>
