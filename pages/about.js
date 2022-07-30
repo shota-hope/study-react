18eafddd2dffa756f2859daf5d30198eb652833f
shota-hope <ddeeppaa.gogo@gmail.com>
2022-07-30 09:45:39 +0900

    export defaultを削除

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,10 @@
 import Head from "next/head";
 import { Footer } from "../components/Footer";
-import Headline from "../components/Headline";
-import {Links} from "../components/Links";
+import { Headline } from "../components/Headline";
+import { Links } from "../components/Links";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export function Home() {
   return (
     <div className={styles.container}>
       <Head>
