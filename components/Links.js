ff4f3c1d7153d209150d96886c908f7adaf3708c
shota-hope <ddeeppaa.gogo@gmail.com>
2022-07-31 15:31:59 +0900

    mapを使いリファクタリング

--- components/Links.js
+++ components/Links.js
@@ -1,33 +1,43 @@
 import styles from "../styles/Home.module.css";
 
+const ITEMS = [
+  {
+    id: 1,
+    href: "https://nextjs.org/docs",
+    title: "Documentation→",
+    description: "Find in-depth information about Next.js features and API.",
+  },
+  {
+    id: 2,
+    href: "hhttps://nextjs.org/learn",
+    title: "Learn→",
+    description: "Learn about Next.js in an interactive course with quizzes!",
+  },
+  {
+    id: 3,
+    href: "https://nextjs.org/docs",
+    title: "Documentation→",
+    description: "Find in-depth information about Next.js features and API.",
+  },
+  {
+    id: 4,
+    href: "https://nextjs.org/docs",
+    title: "Documentation→",
+    description: "Find in-depth information about Next.js features and API.",
+  },
+];
+
 export function Links() {
   return (
     <div className={styles.grid}>
-      <a href="https://nextjs.org/docs" className={styles.card}>
-        <h2>Documentation &rarr;</h2>
-        <p>Find in-depth information about Next.js features and API.</p>
-      </a>
-
-      <a href="https://nextjs.org/learn" className={styles.card}>
-        <h2>Learn &rarr;</h2>
-        <p>Learn about Next.js in an interactive course with quizzes!</p>
-      </a>
-
-      <a
-        href="https://github.com/vercel/next.js/tree/canary/examples"
-        className={styles.card}
-      >
-        <h2>Examples &rarr;</h2>
-        <p>Discover and deploy boilerplate example Next.js projects.</p>
-      </a>
-
-      <a
-        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        className={styles.card}
-      >
-        <h2>Deploy &rarr;</h2>
-        <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-      </a>
+      {ITEMS.map(item => {
+        return (
+          <a key={item.id} href={item.href} className={styles.card}>
+            <h2>{item.title}</h2>
+            <p>{item.description}</p>
+          </a>
+        );
+      })}
     </div>
   );
 }
