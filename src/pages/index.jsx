035c05d81fb38bfae5dec6daf8f288e184b42491
shota-hope <ddeeppaa.gogo@gmail.com>
2022-08-07 11:02:54 +0900

    useCallback,useEffectの第荷引数の説明のため追加

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,22 +3,25 @@ import styles from "../styles/Home.module.css";
 import { Footer } from "../components/Footer";
 import { Main } from "../components/Main";
 import { Header } from "../components/Header";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
   const [count, setCount] = useState(1);
 
-  const handleClick = (e) => {
+  const handleClick = useCallback((e) => {
+    if (count < 10) {
     setCount((count) => count + 1);
-  };
+    }
+  }, [count]);
 
   useEffect(() => {
+    console.log("foo");
     document.body.style.backgroundColor = "lightblue";
 
     return () => {
       document.body.style.backgroundColor = "";
     };
-  }, []);
+  }, [count]);
 
   return (
     <div className={styles.container}>
