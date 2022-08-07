6e8e0b21006e56b0d4e3dc218df9b92418b3c736
shota-hope <ddeeppaa.gogo@gmail.com>
2022-08-07 10:14:46 +0900

    fix: 　バグ修正

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,6 +3,7 @@ import '../styles/globals.css'
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      // eslint-disable-next-line react/jsx-no-undef
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
